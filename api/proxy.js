4b43c96a88f15c6eee5d30e796d7cd470fd87abb
AriffZack <A193193@siswa.ukm.edu.my>
2025-12-11 12:31:34 +0800

    Fix CORS headers to allow Content-Type

--- api/proxy.js
+++ api/proxy.js
@@ -9,8 +9,10 @@ export default async function handler(req, res) {
 
   // 2. Handle CORS (Allow your frontend to call this)
   res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Or your specific domain
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
   
   if (req.method === 'OPTIONS') {
     res.status(200).end();
